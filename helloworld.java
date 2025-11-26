f4cbe58133b56d7215e7fa20b21b8fb31d59b732
varunvallu <varunvallu@gmail.com>
2025-11-27 00:18:56 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -2,6 +2,6 @@
 public class helloworld {
     public static void main(String[] args) {
         // Print message to the console
-        System.out.println("Hello, World!");
+        System.out.println("Hello, devops!");
     }
 }
